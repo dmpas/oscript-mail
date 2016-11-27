c667ac7319548a2566fa047a2d1c0a0d599d5ae6
Sergey Batanov <sergey.batanov@dmpas.ru>
2016-11-27 13:03:45 +0300

    Добавлено описание

--- MailComponent/Properties/AssemblyInfo.cs
+++ MailComponent/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("oscript-component")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Mail Component")]
+[assembly: AssemblyDescription("Почтовая компонента для ОдноСкрипта")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
